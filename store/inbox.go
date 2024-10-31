6123c938a23156343ddc3c4e82e3d7d219c2f752
alalxz <al@ikor.eu.org>
2024-10-31 15:26:40 +0800

    Update inbox.go

--- store/inbox.go
+++ store/inbox.go
@@ -3,7 +3,7 @@ package store
 import (
 	"context"
 
-	storepb "github.com/usememos/memos/proto/gen/store"
+	storepb "github.com/alalxz/memos/proto/gen/store"
 )
 
 // InboxStatus is the status for an inbox.
